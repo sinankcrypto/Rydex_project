bbbc987059dabe835e3eaddb711d575be86e6084
Muhammed Sinan <sinan.46k@gmail.com>
2026-06-05 10:22:35 +0530

    refactor: modify settings to use env variables

--- Rydex/Rydex/settings.py
+++ Rydex/Rydex/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1",
+    cast=lambda v: [s.strip() for s in v.split(",")]
+)
 
 
 # Application definition
